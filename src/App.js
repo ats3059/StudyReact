57e578a0b5c275ad48311735be93084f2e7f2f6d
ats3059 <ats3059@naver.com>
2022-03-27 23:53:58 +0900

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import Button from './Button';
+
+import {useState , useEffect} from 'react';
 
 function App() {
+  const [content,setContent] = useState("");
+  const onChange = (event) => {
+    setContent(event.target.value);
+  }
+  
+  const [todos , setTodos] = useState([]);
+  
+  const submit = (event) => {
+    event.preventDefault();
+    setTodos(curArr => [content,...curArr]);
+    console.log(todos);
+  }
   return (
     <div className="App">
-      <h1>Welcome !!!</h1>
-      <Button text="tomato"/>
+      <h3>todos</h3>
+      <form onSubmit={submit}>
+        <input
+          value={content}
+          onChange={onChange}
+        />
+      </form>
+      <button>submit</button>
+      <li>{todos.map((value)=>value)}</li>  
     </div>
+    
   );
 }
 

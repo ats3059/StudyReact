bf76198a31f21fdf842ade90155eeaed1b31acb7
ats3059 <ats3059@naver.com>
2022-03-28 22:26:31 +0900

    modify app.js

--- src/App.js
+++ src/App.js
@@ -2,34 +2,44 @@
 import {useState , useEffect} from 'react';
 
 function App() {
-  const [content,setContent] = useState("");
-  const onChange = (event) => {
-    setContent(event.target.value);
+  const [coins,setCoins] = useState([]);
+  const [load,setLoad] = useState(true);
+  const [myCoin,setMyCoin] = useState(0);
+  const [conPr,setCoinPr] = useState(0);
+  const onChange = (event) =>{
+    setMyCoin(event.target.value);
   }
-  
-  const [todos , setTodos] = useState([]);
-  
-  const submit = (event) => {
-    event.preventDefault();
-    setTodos(curArr => [content,...curArr]);
-    setContent("");
-  }
+  useEffect(() => {
+    fetch(`https://api.coinpaprika.com/v1/tickers`)
+      .then((res) => res.json())
+      .then((resJson) => {
+        setCoins(resJson);
+        setCoinPr(resJson[0].quotes.USD.price);
+        setLoad(false);
+      })
+  },[])
+ 
+
+
+
   return (
     <div className="App">
-      <h3>todos</h3>
-      <form onSubmit={submit}>
-        <input
-          value={content}
-          onChange={onChange}
-        />
-        <button>submit</button>
-      </form>
-      
-      {todos.map((value)=>
-        {
-          return <li>{value}</li> 
+      <h3>BTC Check : BTC {`length ${coins.length}`}</h3>
+      <h4>{load ? `Loding....` : null}</h4>
+      얼마있뉘?<input value={myCoin} type="number" onChange={onChange}/>
+      <br/>
+      <select hidden={load} onChange={(event)=>setCoinPr(event.target.value)}>
+       {
+        coins.map((coin,index)=> 
+            (
+              <option value={coin.quotes.USD.price} key={index}>{`coinName : ${coin.name} USD :${coin.quotes.USD.price}`}</option>
+            )
+          )
         }
-      )}
+      </select>
+      <br/>
+      현재 소지금으로 구매 가능한 코인의 양은..? {myCoin > 100  ? myCoin/conPr : null}
+   
     </div>
     
   );
